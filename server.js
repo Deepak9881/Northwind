99d26a8061932125693bf1b38fdfb2fb9c2c04d5
Deepak_Mahadev_Shinde <85755523+Deepak9881@users.noreply.github.com>
2023-07-28 12:19:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/hello', function (req, res) {
     res.send(person);
 });
 
-var server = app.listen(8888, function () {
+var server = app.listen(9999, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://localhost:8888", host, port)
